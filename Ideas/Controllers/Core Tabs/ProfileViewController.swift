1e42c6907f186b7ae068e2a302aab451857fb85a
AmrMohamad <amr.mohamed124124@gmail.com>
2023-12-20 12:59:09 +0200

    add sign out button

--- Ideas/Controllers/Core Tabs/ProfileViewController.swift
+++ Ideas/Controllers/Core Tabs/ProfileViewController.swift
@@ -11,9 +11,18 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationController?.navigationBar.prefersLargeTitles = true
-    }
-    
 
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Sign Out",
+            style: .done,
+            target: self,
+            action: #selector(handleSignOutButtonTapped)
+        )
+    }
+
+    @objc private func handleSignOutButtonTapped() {
+        
+    }
 }
--- Ideas/Supported Files/AppDelegate.swift
+++ Ideas/Supported Files/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Amr Mohamad on 11/12/2023.
 //
 
+import FirebaseCore
 import UIKit
 
 @main
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         
         return true
     }

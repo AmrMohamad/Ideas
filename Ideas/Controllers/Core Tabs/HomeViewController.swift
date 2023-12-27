180fd7945b81835d093d93a318e4b2feedb124c7
AmrMohamad <amr.mohamed124124@gmail.com>
2023-12-28 00:44:39 +0200

    Add Navigation bar to CreateNewPostVC

--- Ideas/Controllers/Core Tabs/HomeViewController.swift
+++ Ideas/Controllers/Core Tabs/HomeViewController.swift
@@ -9,13 +9,48 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let composeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.setImage(UIImage(
+            systemName: "square.and.pencil",
+            withConfiguration: UIImage.SymbolConfiguration(pointSize: 18, weight: .medium)
+        ),for: .normal)
+        button.layer.cornerRadius = 30
+        button.layer.shadowColor = UIColor.label.cgColor
+        button.layer.shadowRadius = 14
+        button.layer.shadowOpacity = 0.3
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 //        view.backgroundColor = .red
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        view.addSubview(composeButton)
+        
+        composeButton.addTarget(self, action: #selector(createPost), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        NSLayoutConstraint.activate([
+            composeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -18),
+            composeButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            composeButton.heightAnchor.constraint(equalToConstant: 60),
+            composeButton.widthAnchor.constraint(equalToConstant: 60)
+        ])
     }
 
 
+    @objc func createPost(){
+        let vc = CreateNewPostViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        present(nav, animated: true)
+    }
 }
 
--- Ideas/Controllers/Posts/CreateNewPostViewController.swift
+++ Ideas/Controllers/Posts/CreateNewPostViewController.swift
@@ -11,19 +11,33 @@ class CreateNewPostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .secondarySystemBackground
+        
+        title = "Create Post"
+        setupNavigationButtons()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupNavigationButtons(){
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Post",
+            style: .done,
+            target: self,
+            action: #selector(sendPost)
+        )
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "Cancel",
+            style: .done,
+            target: self,
+            action: #selector(cancelPost)
+        )
     }
-    */
-
+    
+    @objc func sendPost(){
+        
+    }
+    
+    @objc func cancelPost(){
+        dismiss(animated: true)
+    }
 }
